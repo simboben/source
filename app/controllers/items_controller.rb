0f294967d1bd9283c638b6bd8ac9df49fdcb888f
Ben Simpson <ben@wearefixers.co.uk>
2020-05-10 15:05:46 +0100

    added search to items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,18 +1,22 @@
 class ItemsController < ApplicationController
 
-before_action :force_login
+  before_action :force_login
 
+  def index
+    # if there's a search, filter!!!
+    # if not, show all!
 
+    if params[:q].present?
+      # this is where we search!
+      @items = Item.where("lower(title) LIKE ?", "%" + params[:q].downcase + "%")
+    else
+      # show all
+      @items = Item.all
+    end
+  end
 
-def index
-  @items = Item.all
-end
-
-def show
-
-  @item = Item.find(params[:id])
-
-end
-
+  def show
+    @item = Item.find(params[:id])
+  end
 
 end
